a04ee690206ff22fb155e3c756e57621d47dd793
unknown <jayaminisakunthala629@gmail.com>
2022-08-14 19:47:43 +0530

    Content upade with first page controller

---
+++ src/lk/ijse/gdse/chatRoom/controller/ClientHandler.java
@@ -0,0 +1,89 @@
+package lk.ijse.gdse.chatRoom.controller;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable{
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
+    private String clientName;
+    private String message;
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            this.dataInputStream = new DataInputStream(socket.getInputStream());
+            this.clientName = FirstPageFormController.name;
+            clientHandlers.add(this);
+
+            broadcastMessage(clientName+" is connected to the chat...!");
+        } catch (IOException e) {
+            closeAll(socket);
+        }
+    }
+
+    @Override
+    public void run() {
+        String messageFromClient;
+
+        while (socket.isConnected()){
+            try {
+                messageFromClient =dataInputStream.readUTF();
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                closeAll(socket);
+                break;
+            }
+
+    }
+}
+    public void broadcastMessage(String message){
+        serch();
+        for (ClientHandler clientHandler : clientHandlers) {
+            try {
+                String msg = clientHandler.message;
+                clientHandler.dataOutputStream.writeUTF(clientHandler.clientName+" : "+message+"\n");
+                clientHandler.dataOutputStream.flush();
+            } catch (IOException e) {
+                closeAll(socket);
+            }
+        }
+    }
+
+    private void serch() {
+        for (ClientHandler clientHandler:clientHandlers) {
+            Socket s = clientHandler.socket;
+            System.out.println("in for each : "+s);
+            String name = clientHandler.clientName;
+            System.out.println("client name is : "+name);
+            String msg = clientHandler.message;
+            System.out.println("client mesage is : "+msg);
+        }
+    }
+
+    public void removeClientHandler (){
+        clientHandlers.remove(this);
+        broadcastMessage("SERVER: "+clientName+" has left from the chat");
+    }
+
+    private void closeAll(Socket socket) {
+        removeClientHandler();
+
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
--- src/lk/ijse/gdse/chatRoom/controller/FirstPageFormController.java
+++ src/lk/ijse/gdse/chatRoom/controller/FirstPageFormController.java
@@ -5,8 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.DataInputStream;
@@ -18,31 +22,67 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FirstPageFormController implements Initializable {
-    public TextField txtName;
-    public PasswordField pwdPassword;
+    public TextField userName;
+    public AnchorPane mainContext;
+    public Button loginBtn;
 
     ServerSocket serverSocket;
     Socket socket;
-    DataInputStream dataInputStream;
-    DataOutputStream dataOutputStream;
-
-    String message = "";
-
-    public static String userName;
-
-    public void loginOnAction(ActionEvent actionEvent) throws IOException {
-        userName = txtName.getText();
-
-        Parent parent = FXMLLoader.load(getClass().getResource("../views/ClientForm.fxml"));
-        Stage stage2 = new Stage();
-        stage2.setScene(new Scene(parent));
-        stage2.setResizable(false);
-        stage2.show();
-
-    }
+    public static String name;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        userName.requestFocus();
+
+        new Thread(() -> {
+            try {
+                serverSocket = new ServerSocket(2000);
+                System.out.println("Server Started..");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
 
     }
+
+    public void loginAction(ActionEvent actionEvent) throws IOException {
+        goChatPage();
+    }
+
+    public void goChatPage() throws IOException {
+        name = userName.getText();
+
+        ClientFormController.chatName = userName.getText();
+        userName.clear();
+
+        Parent parent = FXMLLoader.load(getClass().getResource("../views/ClientForm.fxml.fxml"));
+        Stage stage1 = new Stage();
+        stage1.setScene(new Scene(parent));
+        stage1.show();
+
+
+        new Thread(() -> {
+            try {
+                socket = serverSocket.accept();
+                System.out.println("Client is connected...!");
+
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }).start();
+    }
+
+    public void enterEvent(KeyEvent keyEvent) throws IOException {
+        if (keyEvent.getCode() == KeyCode.ENTER){
+            goChatPage();
+        }
+    }
+
 }
